b3a2f30cc1457ffa39f078dde49e77bfc2c6eff7
JordanAdams_MacBook <jsadams27@gmail.com>
2024-03-03 14:47:12 -0500

    adds my solution to exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    const stringSplit = string.split('');
+    const revString = stringSplit.reverse();
+    const rejoinString = revString.join('');
 
+    return rejoinString;
 };
 
 // Do not edit below this line
