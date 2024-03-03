b4d6b4e4469e1e183ee61fdbe2438be2329df366
JordanAdams_MacBook <jsadams27@gmail.com>
2024-03-03 14:25:17 -0500

    adds my finished solution to 01 and 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let returnValue = '';
+    
+    if (num < 0) {
+        return 'ERROR';
+    };
+    
+    for (let i = 1; i <= num; i++) {        
+        returnValue += string;
+    } 
+    return returnValue;
 };
 
 // Do not edit below this line
