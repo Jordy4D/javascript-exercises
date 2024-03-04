b20925b2cd397cb8a15ddc1edc2085e5c9fb3296
Jordy4D_LinuxVM <jsadams27@gmail.com>
2024-03-04 16:17:29 -0500

    partial work completed on js exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(startInt, endInt) {
 
+    let sumArray = [];
+    let sum = 0;
+    let arrayStart = parseInt(startInt);
+    let arrayEnd = parseInt(endInt);
+
+
+    if (arrayStart < 0) {
+        return 'ERROR';
+    } else {
+        for (let i = arrayStart; i <= arrayEnd; i++) {
+            sumArray.push(i);
+        }
+    }
+    
+    sumArray.forEach( num => {
+        sum += num;
+    })
+
+    return sum;
 };
 
 // Do not edit below this line
