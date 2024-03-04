a84b375c715b184506a696d92b4dba91d6b18dfc
Jordy4D_LinuxVM <jsadams27@gmail.com>
2024-03-04 09:32:44 -0500

    adds my solution to exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...variable) {
+    
+    return array.filter( ele => !variable.includes(ele) )
 
 };
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
