a59ed90b64b498328721455202d9a2c66b80a37e
Jordy4D_LinuxVM <jsadams27@gmail.com>
2024-03-04 22:41:31 -0500

    adds my over-engineered solution to javascript exercises 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,29 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+
+  let celsTemp = ( (temp - 32) * ( 5/9 ));
+  
+  if (Number.isInteger(celsTemp)) {
+    return parseInt(celsTemp);
+  } else {
+    return parseFloat(celsTemp.toFixed(1));
+  }
+  
+  
+
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  
+  let fahrTemp = ( (temp * ( 9/5 )) + 32 );
+
+  if (Number.isInteger(fahrTemp)) {
+    return parseInt(fahrTemp);
+  } else {
+    return parseFloat(fahrTemp.toFixed(1));
+  }
+
+  
 };
 
 // Do not edit below this line
