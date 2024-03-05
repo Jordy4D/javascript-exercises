2f58ce212b01897240e73cb6b2bad9e71d4d6c9b
JordanAdams_MacBook <jsadams27@gmail.com>
2024-03-04 20:42:48 -0500

    more build out for solution 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,12 +1,27 @@
 const sumAll = function(startInt, endInt) {
 
+    // if ( !isNaN(startInt) || !isNaN(endInt) ) {
+    //     return 'ERROR';
+    // } else {
+    //     continue;
+    // }
+
     let sumArray = [];
     let sum = 0;
-    let arrayStart = parseInt(startInt);
-    let arrayEnd = parseInt(endInt);
+    let arrayStart;
+    let arrayEnd;
+
+    if (startInt > endInt) {
+        arrayEnd = startInt;
+        arrayStart = endInt;
+    } else {
+        arrayStart = startInt;
+        arrayEnd = endInt;
+    }
 
 
-    if (arrayStart < 0) {
+
+    if ( arrayStart < 0 || typeof startInt !== 'string' || typeof endInt !== 'string') {
         return 'ERROR';
     } else {
         for (let i = arrayStart; i <= arrayEnd; i++) {
