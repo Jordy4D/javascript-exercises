d651f1ef930dbc0813c6202fc6e586583737c638
Jordy4D_LinuxVM <jsadams27@gmail.com>
2024-03-15 23:03:16 -0400

    completion of exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let total = arr.reduce((a, b) => a + b, 0);
+  return arr.length > 0 ? total : 0;
+  
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((a, b) => (a * b));
+  
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	// return arr.reduce((a, b) => (a ** b))
+  return (a ** b)
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let f = [];
+  if (num === 0 || num === 1)
+    return 1;
+
+    if (f[num] > 0)
+      return f[num];
+
+    return f[num] = factorial(num - 1) * num;
+  
 };
 
 // Do not edit below this line
