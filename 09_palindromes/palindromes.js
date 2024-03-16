0d93cbbb50de40a28f272454508053fc037ed8ea
JordanAdams_MacBook <jsadams27@gmail.com>
2024-03-16 11:05:09 -0400

    completes exercise 9, palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,22 @@
-const palindromes = function () {
+const palindromes = function (str) {
+
+    //reduce string to letters, remove spaces and puncuation
+    //reduce string to lowercase
+    //create variable with str forward
+    //create variable with str reverse
+    //compare strings to return true or false if not the same
+    //dont forget to join
+
+    let palSplit = str.replace(/[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g, '').toLowerCase().split('')
+    let replaceSpace = palSplit.reduce((a, b) => b === " " ? a : a + b, "")
+    
+    let strForward = replaceSpace;
+    let strReverse = replaceSpace.split('').reverse().join('')
+
+    return strForward === strReverse
 
 };
+
 
 // Do not edit below this line
 module.exports = palindromes;
