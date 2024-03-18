aa98862d7be6d08d7f6259d667d3adc41677e681
JordanAdams_MacBook <jsadams27@gmail.com>
2024-03-18 19:50:04 -0400

    adds my solution to exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,18 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    const d = new Date();
+    let year = d.getFullYear();
+
+    array.forEach(function (obj) {
+        if (obj.yearOfDeath == undefined) {
+            obj.yearOfDeath = year;
+        }
+        
+        obj.age = (obj.yearOfDeath - obj.yearOfBirth)
+
+    })
+
+    array.sort((a, b) => b.age - a.age)
+    return array[0]
 
 };
 
